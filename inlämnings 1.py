5521f60ae8a20e7179690159ca61aa99f945abb6
Legfi <88987985+Legfi@users.noreply.github.com>
2021-09-14 11:39:30 +0200

    fixed the problems

--- inlämnings 1.py
+++ inlämnings 1.py
@@ -2,6 +2,7 @@
 
 
 import random
+from types import resolve_bases
 
 best_score = None
 
@@ -17,33 +18,34 @@ def guessing_game():
 
     while guess != num:
         
-        guess = input( "guess a number between 1 and 10: ")
+        guess = input( "guess a number between 1 and 10: ") 
         try:
             guess = int(guess)
+            if 10 > guess > num:
+                print('try to guess a lower number')
+                tries += 1
+                results.append(guess)
+            elif 0 < guess < num:
+                print ('try to guess a higher number')
+                tries += 1
+                results.append(guess)
+            elif guess == ' ':
+                print('please enter only numbers!')    
+            elif guess == num:
+                global best_score
+                print("Congratulations! You guessed correct!")
+                print("It took you {} tries.".format(tries))
+                if best_score == None or tries < best_score:
+                    best_score = tries
+                results.append(guess)
+                print("best score so far: {} tries".format(best_score))
+                print(results)
+                Play_again()
+            elif guess == 0:
+                break
         except ValueError:
-            guess = int(input('please write only numbers between 1 and 10'))
-
-        if 10 > guess > num:
-            print('try to guess a lower number')
-            tries += 1
-            results.append(guess)
-        elif 0 < guess < num:
-            print ('try to guess a higher number')
-            tries += 1
-            results.append(guess)
-        elif guess == num:
-            global best_score
-            print("Congratulations! You guessed correct!")
-            print("It took you {} tries.".format(tries))
-            if best_score == None or tries < best_score:
-                best_score = tries
-            results.append(guess)
-            print("best score so far: {} tries".format(best_score))
-            print(results)       
-            Play_again()
-        elif guess == 0:
-            break
-
+            print('please enter only numbers!')
+            continue
 def Play_again():
     """this function asks users if they want to play a game or not and save the results in a text file.
     """
@@ -51,17 +53,15 @@ def Play_again():
     if play_again.lower() == "y":  
         guessing_game()
 
-def adding_results(best_score : int):
+def adding_results(best_score : int, first_name_upper : str):
     """this fuction save the best result of the game at a text file.
-
     Args:
         best_score (int): it's the highest sccore of the game.
-
     Returns:
         int: updated high score at the text file.
     """
-    with open("scores.txt", "w") as file:
-        high_score = file.writelines("best score so far: {} tries".format(best_score))
+    with open("scores.txt", "a") as file:
+        high_score = file.writelines("\n"+ "best score: {} tries".format(best_score)+ " by "+ first_name_upper)
         return high_score
 
 
@@ -70,4 +70,4 @@ if __name__ == "__main__":
     user_name = input('Hi Buddy, What can I call you? ')
     first_name_upper = user_name.capitalize()
     guessing_game()
-    adding_results(best_score)
+    adding_results(best_score, first_name_upper)
